3be2029a7fda8e54eda0545f187be3f47aabba13
Rushi Agrawal <rushi.agr@gmail.com>
2016-08-29 01:34:23 +0530

    Bump version.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ dependencies = ['click', 'awscli', 'boto3', 'prettytable', 'future']
 
 setup(
     name='cch',
-    version='0.1.17',
+    version='0.1.18',
     url='https://github.com/rushiagr/cch',
     license='Apache 2.0',
     author='Rushi Agrawal',
